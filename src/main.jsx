159190340644561fecbf35936a20c8dbe871242f
oracle108 <iris.cheng.tw@gmail.com>
2026-04-23 17:55:24 +0800

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from '../oracle108.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  React.createElement(React.StrictMode, null,
+    React.createElement(App, null)
+  )
 )
